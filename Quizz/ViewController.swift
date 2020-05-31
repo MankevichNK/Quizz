e53cfcfa18a6c9b7e797c06df48b21c89f9da631
Mankevich Nadya <nadya.mankevichhh@mail.ru>
2020-06-01 00:39:58 +0300

    Initial Commit

---
+++ Quizz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Quizz
+//
+//  Created by Пользователь on 6/1/20.
+//  Copyright © 2020 Пользователь. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
